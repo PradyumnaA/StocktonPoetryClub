dc1999db17778d118f511cfe7d3afefd752a47cf
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-10-04 14:53:42 -0700

    Rectified errors related to pages and updated routes for About and other pages

---
+++ client/poetry-blog-client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from './home';
+export { default as About } from './about';
+export { default as SignUp } from './SignUp.jsx';
+export { default as SignIn } from './SignIn.jsx';
+export {default as Dashboard} from './Dashboard.jsx';
+export {default as Projects} from './Projects.jsx';
