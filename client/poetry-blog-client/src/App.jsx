728aa1f134d4580549c45e4f9c38541b0ffd4144
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-10-04 18:05:12 -0700

    Header has been created and working

--- client/poetry-blog-client/src/App.jsx
+++ client/poetry-blog-client/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, About, SignIn, SignUp, Dashboard,Projects } from './pages';
+import { Home, About, SignIn, SignUp, Dashboard,Projects,NotFound } from './pages';
+import {Header} from "./Components/index.js";
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/projects" element={<Projects />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
--- client/poetry-blog-client/src/pages/index.js
+++ client/poetry-blog-client/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as SignUp } from './SignUp.jsx';
 export { default as SignIn } from './SignIn.jsx';
 export {default as Dashboard} from './Dashboard.jsx';
 export {default as Projects} from './Projects.jsx';
+export {default as NotFound} from './NotFound.jsx';
